cfcb48e2f37afb3fd6450258faf40152ee45dbf3
Henrod <henrique.93.rodrigues@gmail.com>
2016-10-22 14:54:55 -0200

    Read file

--- src/closeness_centrality/core.clj
+++ src/closeness_centrality/core.clj
@@ -1,11 +1,11 @@
 (ns closeness-centrality.core
-	(:require [clojure.math.numeric-tower :as math]))
+	(:require 
+		[clojure.math.numeric-tower :as math]
+		[clojure.java.io :as io]
+		[clojure.string :as str]))
 
-(def little-graph
-	{1  [2 3]
-	  2 [1 4]
-	  3 [1]
-	  4 [2]})
+(defn as-decimal [m]
+	(zipmap (keys m) (map double (vals m))))
 
 (defn bfs [graph source]
 	(loop [visited #{source}, adj (graph source), e 1, dists 0]
@@ -21,7 +21,7 @@
 	(reduce (fn [m k] (assoc m k (/ 1 (bfs graph k)))) {} (keys graph)))
 
 (defn rank [graph]
-	(sort-by second < (seq (closeness graph))))
+	(sort-by second > (seq (as-decimal (closeness graph)))))
 
 (defn fraudulent [graph source]
 	(if (contains? graph source)
@@ -36,5 +36,16 @@
 						(reduce (fn [m k] (update m k (partial fk e))) ncloseness adj)))))
 		(closeness graph)))
 
-(defn as-decimal [m]
-	(zipmap (keys m) (map double (vals m))))
+(defn read-file
+	[path]
+	(let [txt (slurp path) to-int (fn [s] (Integer. (re-find #"[0-9]+" s)))]
+		(reduce conj [] (map to-int (str/split txt #"(\r\n|\s)")))))
+
+(defn build-graph [input]
+	(let [partial-add (fn [m [src dst]]
+			(if (contains? m src)
+				(update m src #(conj % dst))
+				(assoc m src [dst])))
+		add (fn [m [src dst]] 
+			(partial-add (partial-add m [src dst]) [dst src]))]
+		(reduce add {} (partition 2 input))))
--- test/closeness_centrality/core_test.clj
+++ test/closeness_centrality/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [closeness-centrality.core :refer :all]))
 
+(def little-graph
+	{
+		1 [2 3], 
+		2 [1 4], 
+		3 [1], 
+		4 [2]
+	})
+
 (deftest test-bfs
 	(testing "BFS for little inputs"
 		(are [x y] (= x y)
@@ -24,7 +32,14 @@
 		(let [cmap (closeness little-graph)
 		         rank-seq (rank little-graph)]
 			(and
-				(is (apply <= (map second rank-seq)))
+				(is (apply >= (map second rank-seq)))
+				(is (map (fn [k v] (= (little-graph k) v) (rank-seq little-graph)))))))
+	(testing "Rank for edges.txt"
+		(let [graph (build-graph (read-file "test/closeness_centrality/edges.txt"))
+		         cmap (closeness graph)
+		         rank-seq (rank graph)]
+			(and
+				(is (apply >= (map second rank-seq)))
 				(is (map (fn [k v] (= (little-graph k) v) (rank-seq little-graph))))))))
 
 (deftest test-fraudulent
@@ -57,3 +72,13 @@
 			(= (as-decimal m) {:1 0.5 :2 0.25 :3 0.125})))
 		(is (let [m {1 1/6 "k" 1/3}]
 			(= (as-decimal m) {1 (double 1/6) "k"  (double 1/3)})))))
+
+(deftest test-read-file
+	(testing "read little_input.txt"
+		(is (= [1 2 1 3 2 4]
+			(read-file "test/closeness_centrality/little_input.txt")))))
+
+(deftest test-build-file
+	(testing "build little input"
+		(is (= little-graph 
+			(build-graph (read-file "test/closeness_centrality/little_input.txt"))))))
